3000ec6857139186a6374c71f408c9fa296c652b
Priyansh <priyansh7299@gmail.com>
2018-07-16 16:03:32 +0000

    Update perceptron.py

--- perceptron.py
+++ perceptron.py
@@ -1,23 +1,14 @@
-def train_model():
-    n=2
-    weight = [0 for s in range(n)]
-    bias = 0
-    threshold = 0.2
-    alpha = 1
-    n_weight, n_bias = [0 for s in range(n)], 0
-    inputs=[[1,1], [1,0], [0,1], [0,0]]
-    targets=[1,-1,-1,-1]
-    delta_w = [ [ 0 for a in range(n+1) ] for b in range(4) ]
-    x=[]
+def train_model(n, threshold, alpha, inputs, targets, n_tp):
+    weight, bias = [0 for s in range(n)], 0
+    n_weight, n_bias = [0 for s in range(n)], 0    
+    delta_w = [ [ 0 for a in range(n+1) ] for b in range(n_tp) ]
     t=0
     e=0
     while True:
-        print("\nEpoch : "+str(e+1))
         e+=1
         #for each Training Pair
-        for tp in range(4):
-            x = inputs[tp]
-            t = targets[tp]
+        for tp in range(n_tp):
+            x, t = inputs[tp], targets[tp]
 
             #compute the response
             sum=0
@@ -32,6 +23,7 @@ def train_model():
                 y=0
             elif y_in<(-threshold):
                 y=-1
+
             #updating weights and biases
             if not y==t:
                 for i in range(n):
@@ -41,10 +33,7 @@ def train_model():
                 for i in range(n):
                     n_weight[i]=weight[i]
                 n_bias=bias
-
-            print("new weight : "+str(n_weight))
-            print("weight : "+str(weight))    
-
+            
             #create delta_w matrix
             for z in range(n):
                 delta_w[tp][z] = n_weight[z] - weight[z]
@@ -53,10 +42,10 @@ def train_model():
             for i in range(n):
                 weight[i]=n_weight[i]
             bias=n_bias
-        print("weight change : "+str(delta_w))            
+
         #checking weight change    
         training=0  
-        for p in range(4):
+        for p in range(n_tp):
             for q in range(n+1):
                 if not delta_w[p][q]==0:
                     training=1
@@ -64,6 +53,26 @@ def train_model():
             if training==1:
                 break
         if training==0:
-            return                    
+            return weight, bias, e 
 
-train_model()
+
+inputs=[[1,1], [1,-1], [-1,1], [-1,-1]]
+targets=[1,-1,-1,-1]
+weights, bias, epoch = train_model(2, 0.2, 1, inputs, targets, 4)
+print(weights, bias, epoch)
+
+
+x = [0,0]
+
+sum = 0
+for i in range(2):
+    sum+=x[i]*weights[i]
+y_in = bias + sum
+
+if y_in>0.2:
+    y=0
+elif y_in>=-0.2 and y_in<=0.2:
+    y=0
+elif y_in<-0.2:
+    y=-1
+print(y)            
